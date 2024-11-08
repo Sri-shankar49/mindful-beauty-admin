b238f782094a9474cfb6363e6b3e7fa02163ebcb
Sri-shankar49 <srishankar4499@gmail.com>
2024-11-08 20:04:49 +0530

    commit feat : vercel error rectified

--- mindfulBeautyAdmin/src/pages/RatingsReviews.tsx
+++ mindfulBeautyAdmin/src/pages/RatingsReviews.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export const RatingsReviews = () => {
   return (
     <div>RatingsReviews</div>
--- mindfulBeautyAdmin/src/pages/SalesTransactions.tsx
+++ mindfulBeautyAdmin/src/pages/SalesTransactions.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export const SalesTransactions = () => {
   return (
     <div>SalesTransactions</div>
