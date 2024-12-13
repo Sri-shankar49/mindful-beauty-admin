9ac5581326a2b04e181431a2fdcdb77e1b7877d0
Sri-shankar49 <srishankar4499@gmail.com>
2024-12-13 17:45:08 +0530

    commit feat : django URL changed

--- mindfulBeautyAdmin/src/api/apiUrl.tsx
+++ mindfulBeautyAdmin/src/api/apiUrl.tsx
@@ -2,9 +2,9 @@
 import axios from 'axios';
 
 export const apiUrl = {
-  apiUrlConfig: "http://192.168.1.7:8000",
+  // apiUrlConfig: "http://192.168.1.7:8000",
   // apiUrlConfig: "http://192.168.43.150:8000", Motorola One Power
-  // apiUrlConfig: "https://djangoapp-mindful-beautys-projects.vercel.app",
+  apiUrlConfig: "https://djangoapp-mindful-beautys-projects.vercel.app",
 }
 
 // Create an Axios instance with the base URL
