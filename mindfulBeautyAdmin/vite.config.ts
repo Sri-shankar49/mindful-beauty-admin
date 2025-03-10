41e733a6210572e93b0e2529f938b4ba95387db2
Sri-shankar49 <srishankar4499@gmail.com>
2025-03-10 19:54:24 +0530

    commit feat : vercel error rectified roll-on.

--- mindfulBeautyAdmin/vite.config.ts
+++ mindfulBeautyAdmin/vite.config.ts
@@ -1,33 +1,13 @@
-// import path from "path"
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// })
-
-import { fileURLToPath } from "url";
-import path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
-// Fix for ESM environments
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import path from "path"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"), // Enables `@` as an alias for `src/`
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-});
-
+})
