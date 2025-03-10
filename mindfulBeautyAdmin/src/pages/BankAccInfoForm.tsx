768975bfad2a843c076cc428b891229807df13e2
Sri-shankar49 <srishankar4499@gmail.com>
2025-03-10 19:41:24 +0530

    commit feat : vercel error rectified roll-on.

--- mindfulBeautyAdmin/src/pages/BankAccInfoForm.tsx
+++ mindfulBeautyAdmin/src/pages/BankAccInfoForm.tsx
@@ -5,7 +5,7 @@ import { InputField } from '@/common/InputField';
 import { Button } from '@/common/Button';
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import * as zod from "zod";
+import zod from "zod";
 import { bankAccInfo } from "@/api/apiConfig";
 
 // Define Zod schema for validation
